eb0b356032be18b01c09fd4b38aeb81d1716bbd4
locbh <locbh3010@gmail.com>
2026-06-26 20:13:36 +0700

    refacctor ui appbar

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,13 @@
 import { Stack } from 'expo-router';
 import { MD3DarkTheme, PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <PaperProvider theme={MD3DarkTheme} settings={{ rippleEffectEnabled: true }}>
+        <StatusBar style="light" />
         <Stack
           screenOptions={{
             headerShown: false,
